bd73dbdf27109daf8ec980fac29338945676f92c
Akshat Kumar <akshatkumar.dev@gmail.com>
2020-03-10 18:51:38 +0530

    Got the length of each video in the playlist

--- scraping.py
+++ scraping.py
@@ -9,4 +9,20 @@ while(1):
         break
     res = requests.get(res)
     pret = bs4.BeautifulSoup(res.text,'html.parser')
-    print(pret)
+    x = []
+    hours = 0
+    minutes = 0
+    seconds = 0
+    secondArray = []
+    minutesArray = []
+    hoursArray = []
+    allSpan = pret.find_all('span')
+    for i in allSpan:
+        temp = i.get("aria-label")
+        if(type(" ") == type(temp)):
+            x.append(i)
+        
+    for i in range(4):
+        x.pop(0)
+    print(x)
+    print(len(x))
